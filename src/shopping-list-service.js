36ecff5b37286eab92888e6826e9d55332175bbc
Evan Vogts <evanjvogts@gmail.com>
2019-11-07 10:02:45 -0600

    final

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,20 +1,33 @@
 const ShoppingListService = {
-  getAllShoppingItems(knex) {
-    return knex.select('*').from('shopping_list');
+  getAllItems(knex) {
+    return knex
+      .select('*')
+      .from('shopping_list');
   },
   getById(knex, id) {
-    return knex.from('shopping_list').select('*').where('id', id).first();
-  },
-  insertItem(knex, newItem) {
-    return knex.insert(newItem).into('shopping_list').returning('*').then(rows => rows[0]);
-  },
-  updateItem(knex, id, newData) {
-    return knex('shopping_list').where({ id }).update(newData);
+    return knex
+      .from('shopping_list')
+      .select('*')
+      .where('id', id)
+      .first();
   },
   deleteItem(knex, id) {
-    return knex('shopping_list').where({ id }).delete();
-  }
+    return knex('shopping_list')
+      .where({ id })
+      .delete();
+  },
+  updateItem(knex, id, newItemFields) {
+    return knex('shopping_list')
+      .where({ id })
+      .update(newItemFields);
+  },
+  insertItem(knex, newItem) {
+    return knex
+      .insert(newItem)
+      .into('shopping_list')
+      .returning('*')
+      .then(rows => rows[0]);
+  },
 };
-
 
 module.exports = ShoppingListService;
